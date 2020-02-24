f9f986a54d9cdf5d953d1545291c8f870400c930
Adroaldo J. Pagliari <adroaldo@outlook.com>
2020-02-24 15:56:31 -0300

    api node for products usage

---
+++ node-api/src/models/Product.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const paginate = require("mongoose-paginate");
+
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  createAt: {
+    type: Date,
+    default: Date.now,
+  }
+})
+
+mongoose.plugin(paginate);
+mongoose.model('Product', productSchema);
